86b04cce4eda1e3f1d008e64b7379c24f2f2afd2
Mirda Ganesh <134841315+mirdaganesh55@users.noreply.github.com>
2024-06-12 17:11:18 +0530

    Delete PlasticCompany directory

--- PlasticCompany/src/main/java/com/java/employ/EmployDAO.java
+++
@@ -1,9 +0,0 @@
-package com.java.employ;
-
-import java.io.IOException;
-
-public interface EmployDAO {
-
-	public String saveEmpDetailsDao(Employ employ,EmpLogin empLogin) throws IOException;
-	public String empLoginDao(EmpLogin login);
-}
